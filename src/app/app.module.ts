bfd76befa93c6b219dc8e16e2ff15979b3e9ae87
Alison Chancay <chancayalison@gmail.com>
2023-11-20 22:26:09 -0500

    Alison Ingresar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/register/register.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    IngresarComponent
   ],
   imports: [
     BrowserModule,
