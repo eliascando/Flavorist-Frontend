6c54b97f2eea8695b2bf24a9354946e82e80aba4
Lindsay Villegas Licoa <villegas.licoa@gmail.com>
2023-11-22 00:34:45 -0500

    avance componente feedPrincipal

---
+++ src/app/components/feed-principal/feed-principal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-principal',
+  templateUrl: './feed-principal.component.html',
+  styleUrls: ['./feed-principal.component.css']
+})
+export class FeedPrincipalComponent {
+
+}
